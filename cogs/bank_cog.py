d9cbc6a88957ba5d2cdb1e2d64123a7f48781357
scorpion2000 <robert.bartha1@gmail.com>
2026-05-21 00:22:17 +0200

    Added banking functionality back

---
+++ cogs/bank_cog.py
@@ -0,0 +1,41 @@
+import discord
+
+from discord.ext import commands
+from discord import app_commands
+
+
+class EconomyCog(commands.Cog):
+
+    def __init__(self, bot):
+
+        self.bot = bot
+        self.economy = bot.economy_service
+
+    @app_commands.command(
+        name="send_bits",
+        description="Send your bits to someone else!"
+    )
+    async def send_bits(
+        self,
+        interaction: discord.Interaction,
+        to_user: discord.User,
+        amount: int
+    ):
+        # Oh jesus python looks scuffed
+        sender = interaction.user
+
+        success, error = self.economy.transfer_bits(sender.name,to_user.name,amount)
+
+        if not success:
+            await interaction.response.send_message(error,ephemeral=True)
+            return
+
+        await interaction.response.send_message(
+            f"{sender.mention} sent "
+            f"{amount} bits to "
+            f"{to_user.mention}!"
+        )
+
+
+async def setup(bot):
+    await bot.add_cog(EconomyCog(bot))
---
+++ services/economy_service.py
@@ -0,0 +1,44 @@
+import json
+
+class EconomyService:
+
+    def __init__(self):
+        self.bank = self.load_bank()
+
+    def load_bank(self):
+        try:
+            with open("bit_bank.json", "r") as fp:
+                return json.load(fp)
+        except FileNotFoundError:
+            return {}
+    
+    def save_bank(self):
+        with open("bit_bank.json", "w") as fp:
+            json.dump(self.bank, fp)
+
+    
+    def get_balance(self, username):
+        return self.bank.get(username, 0)
+
+    def add_bits(self, username, amount):
+        if self.bank.get(username, 0) + bits < 0:
+            return False
+        self.bank.update({username : self.bank.get(username, 0) + bits})
+        self.save_bank()
+        return True
+
+    def transfer_bits(self, sender, receiver, amount):
+        if amount <= 0:
+            return False, "Amount must be positive."
+
+        sender_balance = self.get_balance(sender)
+
+        if sender_balance < amount:
+            return False, (
+                f"You only have {sender_balance} bits!"
+            )
+
+        self.add_bits(sender, -amount)
+        self.add_bits(receiver, amount)
+
+        return True, None
